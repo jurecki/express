b705491e3c7ccbbc98e6e5d36200e58ab053e810
Pawel Jurecki <jurecki.pawel@gmail.com>
2020-04-30 14:54:54 +0200

    Add dark layout of aapps

--- index.js
+++ index.js
@@ -1,34 +1,46 @@
 const express = require('express');
-const path = require('path')
-
+const path = require('path');
+const hbs = require('express-handlebars');
 const app = express();
+
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
 app.listen(8000, () =>
     console.log('server is running at port 8000')
 )
 
-app.use((req, res, next) => {
-    res.show = (name) => {
-        res.sendFile(path.join(__dirname, `/views/${name}`))
-    }
-    next()
-})
-
 app.use(express.static(path.join(__dirname, '/public')))
 
 app.use('/user', (req, res, next) => {
-    res.show('forbidden.html')
+    res.render('forbidden')
     next()
 })
 
+app.get('/hello/:name', (req, res) => {
+    res.render('hello', { name: req.params.name })
+})
+
 app.get('/', (req, res) => {
-    res.show('index.html')
+    res.render('index')
 })
 
 app.get('/about', (req, res) => {
-    res.show('about.html')
+    res.render('about', { layout: 'dark' })
+})
+
+app.get('/contact', (req, res) => {
+    res.render('contact')
+})
+
+app.get('/info', (req, res) => {
+    res.render('info')
+})
+
+app.get('/history', (req, res) => {
+    res.render('history');
 })
 
 app.use((req, res, next) => {
-    res.status(404).show('error.html')
+    res.status(404).render('error')
 })
